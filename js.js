0a47752aa1bc88040c943798bafd1eddbb89e335
Lahyazineb <99026061+Lahyazineb@users.noreply.github.com>
2022-02-18 22:38:08 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,40 @@
+
+   const helloSpeaker = {
+	speakWord: 'Hello',	
+};
+//This says Hello
+(function () {
+	helloSpeaker.speak = function (userName) {
+		return (helloSpeaker.speakWord + " " + userName);
+	}
+}());
+
+
+//2
+/*Goodbye speaker function*/
+const byeSpeaker = {
+	speakWord: 'Good Bye'
+};
+
+(function () {
+	byeSpeaker.speak = function (userName) {
+		return (byeSpeaker.speakWord + " " + userName);
+	}
+}());
+
+//3
+//This choose the names
+(function () {
+	var names =["Zineb", "ayoub", "Jihane", "jad", "laila", "saif", "joli", "aya", "dora", "Jim"];
+	for (var i = 0; i < names.length; i++) {
+		var name = names[i];
+		var firstLetter = name.charAt(0).toLowerCase();
+		if (firstLetter === 'j') {
+			console.log(byeSpeaker.speak(name));
+			
+		} else {
+			console.log(helloSpeaker.speak(name));
+		
+		}
+	}
+})();
